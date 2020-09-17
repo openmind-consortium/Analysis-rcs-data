9b629050909cf391a04ff963682f43d43637766b
kristinksellers <kristinksellers@gmail.com>
2020-09-16 19:15:12 -0700

    Update getSampleRateAcc.m
    Initial commit

--- code/getSampleRateAcc.m
+++ code/getSampleRateAcc.m
@@ -1,22 +1,14 @@
 function sratesout = getSampleRateAcc(srates)
-%% input: matrix of sample rates of each packet from TimeDomainData.Json 
-%% output: sample rate in Hz 
-
+%%
+% Input: matrix of sample rates of each packet from RawDataAccel.Json 
+% Output: sample rate in Hz 
+%%
 
 if length(unique(srates)) > 1 
     warning('you have non uniform sample rates in your data'); 
 end
 
-
-
-
-fprintf('out of %d packets:\n\t%d packets at 250Hz\n\t%d packets at 500Hz\n \t%d packets at 1000Hz\n',...
-    length(srates),...
-    sum((srates==0)),...
-    sum((srates==1)),...
-    sum((srates==2)));
-
-% make sample rates numbers 
+% Convert sample rate codes to Hz
 sratesout = srates; 
 sratesout(srates==0) = 64;
 sratesout(srates==1) = 32;
