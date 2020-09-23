bc30ea52008cedb324aa5e3355d4955dde804175
kristinksellers <kristinksellers@gmail.com>
2020-09-23 15:54:18 -0700

    Update getSampleRate.m
    Updated function input description

--- code/getSampleRate.m
+++ code/getSampleRate.m
@@ -1,19 +1,24 @@
 function sratesout = getSampleRate(srates)
-%% input: matrix of sample rates of each packet from TimeDomainData.Json 
-%% output: sample rate in Hz 
+%%
+% Input: Vector of sample rates of each packet from RawDataTD.json
+% (Vector of the 'SampleRate' column, after having imported json
+% into Matlab table). Typical workflow is to call this function from
+% createTimeDomainTable.m. 
+%
+% Output: sample rate in Hz
+%%
 
-
-if length(unique(srates)) > 1 
-    warning('you have non uniform sample rates in your data'); 
+if length(unique(srates)) > 1
+    warning('you have non uniform sample rates in your data');
 else
-    sratenum = unique(srates); 
-    switch sratenum 
-        case 0 
-            srate = 250; % sample rate in Hz. 
-        case 1 
-            srate = 500; 
-        case 2 
-            srate = 1e3; 
+    sratenum = unique(srates);
+    switch sratenum
+        case 0
+            srate = 250; % sample rate in Hz.
+        case 1
+            srate = 500;
+        case 2
+            srate = 1e3;
     end
 end
 fprintf('out of %d packets:\n\t%d packets at 250Hz\n\t%d packets at 500Hz\n \t%d packets at 1000Hz\n',...
@@ -21,8 +26,8 @@ fprintf('out of %d packets:\n\t%d packets at 250Hz\n\t%d packets at 500Hz\n \t%d
     sum((srates==0)),...
     sum((srates==1)),...
     sum((srates==2)));
-% make sample rates numbers 
-sratesout = srates; 
+% make sample rates numbers
+sratesout = srates;
 sratesout(srates==0) = 250;
 sratesout(srates==1) = 500;
 sratesout(srates==2) = 1000;
