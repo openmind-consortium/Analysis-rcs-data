737a3c2c3b37f6eaa4b6ed69309ffaa1d24e592b
kristinksellers <kristinksellers@gmail.com>
2020-10-21 13:08:24 -0700

    Functions for creating new rows of data
    Functions to create new rows of complete data (to be used to populate intermediate tables -- TD_SettingTable, Power_SettingsTable, FFT_SettingsTable)

---
+++ code/addNewEntry_FFTSettings.m
@@ -0,0 +1,30 @@
+function [newEntry,fftConfig] = addNewEntry_FFTSettings(actionType,recNum,currentSettings,TDsettings,fftConfig)
+%%
+% Extract FFT settings data in order to add a new row to the
+% FFT_SettingsTable; ensures that all table fields are filled for each
+% entry (otherwise warning will print)
+%
+%%
+HostUnixTime = currentSettings.RecordInfo.HostUnixTime;
+
+newEntry.action = actionType;
+newEntry.recNum = recNum;
+newEntry.time = HostUnixTime;
+
+% Get fftConfig info if updated
+
+if isfield(currentSettings,'SensingConfig') && isfield(currentSettings.SensingConfig,'fftConfig')
+    fftConfig = currentSettings.SensingConfig.fftConfig;
+end
+newEntry.fftConfig = fftConfig;
+
+% Get sample rate for each TD channel; all TD channels have
+% same Fs (or is listed as NaN)
+for iChan = 1:4
+    TDsampleRates(iChan) = str2double(TDsettings(iChan).sampleRate(1:end-2));
+end
+TDsampleRates = unique(TDsampleRates);
+currentTDsampleRate = TDsampleRates(~isnan(TDsampleRates));
+newEntry.TDsampleRates = currentTDsampleRate;
+
+end
