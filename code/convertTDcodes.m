63a4af46c68882bd29e32afba7a286ddcf69e497
kristinksellers <kristinksellers@gmail.com>
2021-01-27 14:52:08 -0800

    Update convertTDcodes.m
    Converting gain and HPF to human-readable vallues; saving other numbers as numbers and not strings

--- code/convertTDcodes.m
+++ code/convertTDcodes.m
@@ -13,27 +13,51 @@ function TDsettings = convertTDcodes(TDdata)
 
 TDsettings = TDdata;
 for iChan = 1:length(TDsettings)
+    % Gain
+    switch TDdata(iChan).gain
+        case 0
+            TDsettings(iChan).gain = 500;
+        case 1
+            TDsettings(iChan).gain = 1000;
+        case 2
+            TDsettings(iChan).gain = 250;
+        case 4
+            TDsettings(iChan).gain = 2000;
+    end
+    
+    % HPF
+    switch TDdata(iChan).hpf
+        case 0
+            TDsettings(iChan).hpf = 8.5;
+        case 16
+            TDsettings(iChan).hpf = 1.2;
+        case 32
+            TDsettings(iChan).hpf = 3.3;
+        case 96
+            TDsettings(iChan).hpf = 8.6;
+    end
+    
     % LFP 1 (front end)
     switch TDdata(iChan).lpf1
         case 9
-            TDsettings(iChan).lpf1 = '450Hz';
+            TDsettings(iChan).lpf1 = 450;
         case 18
-            TDsettings(iChan).lpf1 = '100Hz';
+            TDsettings(iChan).lpf1 = 100;
         case 36
-            TDsettings(iChan).lpf1 = '50Hz';
+            TDsettings(iChan).lpf1 = 50;
         otherwise
             TDsettings(iChan).lpf1 = 'unexpected';
     end
     % LFP 1 (back end amplifier)
     switch TDdata(iChan).lpf2
         case 9
-            TDsettings(iChan).lpf2 = '100Hz';
+            TDsettings(iChan).lpf2 = 100;
         case 11
-            TDsettings(iChan).lpf2 = '160Hz';
+            TDsettings(iChan).lpf2 = 160;
         case 12
-            TDsettings(iChan).lpf2 = '350Hz';
+            TDsettings(iChan).lpf2 = 350;
         case 14
-            TDsettings(iChan).lpf2 = '1700Hz';
+            TDsettings(iChan).lpf2 = 1700;
         otherwise
             TDsettings(iChan).lpf2 = 'unexpected';
     end
@@ -98,11 +122,11 @@ for iChan = 1:length(TDsettings)
     % Sample rate
     switch TDdata(iChan).sampleRate
         case 0
-            TDsettings(iChan).sampleRate = '250Hz';
+            TDsettings(iChan).sampleRate = 250;
         case 1
-            TDsettings(iChan).sampleRate = '500Hz';
+            TDsettings(iChan).sampleRate = 500;
         case 2
-            TDsettings(iChan).sampleRate = '1000Hz';
+            TDsettings(iChan).sampleRate = 1000;
         case 240
             TDsettings(iChan).sampleRate = 'disabled';
         otherwise
@@ -110,7 +134,7 @@ for iChan = 1:length(TDsettings)
     end
     TDsettings(iChan).chanOut = sprintf('+%s-%s',...
         TDsettings(iChan).plusInput,TDsettings(iChan).minusInput);
-    TDsettings(iChan).chanFullStr = sprintf('%s LFP1-%s LFP2-%s SR-%s',...
+    TDsettings(iChan).chanFullStr = sprintf('%s LFP1-%i LFP2-%i SR-%i',...
         TDsettings(iChan).chanOut,...
         TDsettings(iChan).lpf1,TDsettings(iChan).lpf2,TDsettings(iChan).sampleRate);
 end
