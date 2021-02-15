428d57185dc3f37cd8d66c59099aa6f158a2f2d2
kristinksellers <kristinksellers@gmail.com>
2021-02-15 15:59:26 -0800

    Update top-level function and script names

--- code/DEMO_LoadRCS.m
+++ code/DEMO_LoadRCS.m
@@ -13,39 +13,3 @@ load([pathName fileName])
 % time base
 dataStreams = {timeDomainData, AccelData, PowerData, FFTData, AdaptiveData};
 [combinedDataTable] = createCombinedTable(dataStreams,unifiedDerivedTimes,metaData);
-
-%%
-% This cell creates debugTable - containing only timing information from
-% selected dataStreams for testing
-% Convert sparse matrics (with only time variables) to tables
-if ~isempty(timeDomainData_onlyTimeVariables)
-    [timeDomainData_onlyTimeVariables] = createTableFromSparseMatrix(timeDomainData_onlyTimeVariables,timeDomain_timeVariableNames);
-    timeDomainData_onlyTimeVariables.TimeDomainStream = ones(size(timeDomainData_onlyTimeVariables,1),1);
-end
-
-if ~isempty(AccelData_onlyTimeVariables)
-    [AccelData_onlyTimeVariables] = createTableFromSparseMatrix(AccelData_onlyTimeVariables,Accel_timeVariableNames);
-    AccelData_onlyTimeVariables.AccelStream = ones(size(AccelData_onlyTimeVariables,1),1);
-end
-
-if ~isempty(PowerData_onlyTimeVariables)
-    [PowerData_onlyTimeVariables] = createTableFromSparseMatrix(PowerData_onlyTimeVariables,Power_timeVariableNames);
-    PowerData_onlyTimeVariables.PowerStream = ones(size(PowerData_onlyTimeVariables,1),1);
-end
-
-if ~isempty(FFTData_onlyTimeVariables)
-    [FFTData_onlyTimeVariables] = createTableFromSparseMatrix(FFTData_onlyTimeVariables,FFT_timeVariableNames);
-    FFTData_onlyTimeVariables.FFTStream = ones(size(FFTData_onlyTimeVariables,1),1);
-end
-
-if ~isempty(AdaptiveData_onlyTimeVariables)
-    [AdaptiveData_onlyTimeVariables] = createTableFromSparseMatrix(AdaptiveData_onlyTimeVariables,Adaptive_timeVariableNames);
-    AdaptiveData_onlyTimeVariables.AdaptiveStream = ones(size(AdaptiveData_onlyTimeVariables,1),1);
-end
-
-timeDataStreams = {timeDomainData_onlyTimeVariables, AccelData_onlyTimeVariables,...
-    PowerData_onlyTimeVariables, FFTData_onlyTimeVariables, AdaptiveData_onlyTimeVariables};
-
-% Create debug table
-[debugTable] = createCombinedTable_debugTable(timeDataStreams,unifiedDerivedTimes,metaData);
-%%
