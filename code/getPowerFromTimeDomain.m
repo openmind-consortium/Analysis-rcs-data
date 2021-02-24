0fa1670fd4ecd206676470176ea4b41a7b2933ab
Juan Anso <juan.anso@gmail.com>
2021-02-24 09:12:28 -0800

    Fix getPowerFromTimeDomain.m can handle a session with more than one recording number (stop and start streaming), also added hannWindow.m to account for hann window 25%, 50% and 100%

--- code/getPowerFromTimeDomain.m
+++ code/getPowerFromTimeDomain.m
@@ -1,4 +1,4 @@
-function  [powerFromTimeDomain] = getPowerFromTimeDomain(combinedDataTable,fftSettings, powerSettings, metaData)
+function  [powerOutput] = getPowerFromTimeDomain(combinedDataTable,fftSettings, powerSettings, metaData)
 % creates a table with power computed signals from time domain signal using
 % an equivalent process to the internal power computation in the device
 % power is computed using
@@ -20,62 +20,86 @@ function  [powerFromTimeDomain] = getPowerFromTimeDomain(combinedDataTable,fftSe
 % - no change in fft and power settings in data set
 %
 
-% initialize output power 
-powerFromTimeDomain = table();
-powerFromTimeDomain.localTime= combinedDataTable.localTime;
-powerFromTimeDomain.DerivedTimes = combinedDataTable.DerivedTime;
-for inumBands = 1:8 % initialize bands
-    powerFromTimeDomain.(['PowerCh',num2str(inumBands)]) = nan(1,size(combinedDataTable,1))';
-end
+powerOutput = table();
+% powerFromTimeDomain = table(size(powerSettings,1),size(powerSettings,2)+1);
+% powerFromTimeDomain = powerSettings;
+for inumRec = 1:size(powerSettings,1)
 
-% loop for each time domain channel
-for c=1:4
-    % extract input parameters
-    tch = combinedDataTable.DerivedTime;
-    t = seconds(tch-tch(1))/1000;
-    [interval,binStart,binEnd,fftSize] = readFFTsettings(powerSettings,c);
-    sr = fftSettings.TDsampleRates;
-    switch fftSize % actual fft size of device for 64, 250, 1024 fftpoints
-        case 64, fftSizeActual = 62;
-        case 256, fftSizeActual = 250;
-        case 1024, fftSizeActual = 1000;
+    % search power subdataset in the session 
+    timeFormat = sprintf('%+03.0f:00',metaData.UTCoffset);
+    timeStart = powerSettings.timeStart(inumRec);     
+    localTimeStart = datetime(timeStart/1000,'ConvertFrom','posixTime','TimeZone',timeFormat,'Format','dd-MMM-yyyy HH:mm:ss.SSS');
+    timeStop = powerSettings.timeStop(inumRec);
+    localTimeStop = datetime(timeStop/1000,'ConvertFrom','posixTime','TimeZone',timeFormat,'Format','dd-MMM-yyyy HH:mm:ss.SSS');
+    idxRecordA = find(combinedDataTable.localTime >= localTimeStart);
+    idxRecordB = find(combinedDataTable.localTime <= localTimeStop);
+    [C,idA,idB] = intersect(idxRecordA,idxRecordB);
+    idxRecordUse = C(idA);
+    
+    % initialize output power 
+    powerFromTimeDomain = table();
+    powerFromTimeDomain.localTime= combinedDataTable.localTime(idxRecordUse);
+    powerFromTimeDomain.DerivedTimes = combinedDataTable.DerivedTime(idxRecordUse);
+    
+    for inumBands = 1:8 % initialize bands
+        powerFromTimeDomain.(['PowerCh',num2str(inumBands)]) = nan(1,length(idxRecordUse))';
     end
-    keych = combinedDataTable.(['TD_key',num2str(c-1)]); % next channel
-    td_rcs = transformTDtoRCS(keych,metaData.ampGains.(['Amp',num2str(c)])); % transform TD signal to rcs internal values
-    overlap = 1-(sr*interval/1e3/fftSizeActual); % time window parameters
-    L = fftSize; % timeWin is now named L, number of time window points
-    hann_win = hannWindow(L,fftSettings.fftConfig.windowLoad);
-    stime = 1; % sample 1 of data set where window starts
-    totalTimeWindows = ceil(length(td_rcs)/L/(1-overlap)); 
-    counter = 1; % initialize counter
-    while counter <= totalTimeWindows % loop through time singal
-        if stime+L <= length(t) % check at least one time window available before reach end signal
-            X = fft(td_rcs(stime:stime+L-1)'.*hann_win,fftSize); % fft of the next window
-            SSB = X(1:L/2); % from double to single sided FFT
-            SSB(2:end) = 2*SSB(2:end); % scaling step 1, multiply by 2 bins 2 to end (all except DC)
-            YFFT = abs(SSB/(L/2)); % scaling step 2, dividing by fft buffer size (L/2) (to be hoest i think this should be L)
-            fftPower = 2*(YFFT.^2); % this factor 2 is necessary to match power values from RCS
-            binStartBand1 = binStart(2*c-1);
-            binEndBand1 = binEnd(2*c-1);
-            binStartBand2 = binStart(2*c);
-            binEndBand2 = binEnd(2*c);
-            powerFromTimeDomain.(['PowerCh',num2str(2*c-1)])(stime+L-1) = sum(fftPower(binStartBand1:binEndBand1));
-            powerFromTimeDomain.(['PowerCh',num2str(2*c)])(stime+L-1) = sum(fftPower(binStartBand2:binEndBand2));
+
+    % loop for each time domain channel
+    for c=1:4
+        % extract input parameters
+        tch = combinedDataTable.DerivedTime(idxRecordUse);
+        t = seconds(tch-tch(1))/1000;
+        [interval,binStart,binEnd,fftSize] = readFFTsettings(powerSettings,inumRec);
+        sr = fftSettings.TDsampleRates;
+        switch fftSize % actual fft size of device for 64, 250, 1024 fftpoints
+            case 64, fftSizeActual = 62;
+            case 256, fftSizeActual = 250;
+            case 1024, fftSizeActual = 1000;
         end
-        counter = counter + 1;
-        stime = stime + (L - ceil(L*overlap));
-    end   
-end
+        keych = combinedDataTable.(['TD_key',num2str(c-1)]); % next channel
+        keychUse = keych(idxRecordUse);
+        td_rcs = transformTDtoRCS(keychUse,metaData.ampGains.(['Amp',num2str(c)])); % transform TD signal to rcs internal values
+        overlap = 1-(sr*interval/1e3/fftSizeActual); % time window parameters
+        L = fftSize; % timeWin is now named L, number of time window points
+        hann_win = hannWindow(L,fftSettings.fftConfig.windowLoad);
+        stime = 1; % sample 1 of data set where window starts
+        totalTimeWindows = ceil(length(td_rcs)/L/(1-overlap)); 
+        counter = 1; % initialize counter
+        while counter <= totalTimeWindows % loop through time singal
+            if stime+L <= length(t) % check at least one time window available before reach end signal
+                X = fft(td_rcs(stime:stime+L-1)'.*hann_win,fftSize); % fft of the next window
+                SSB = X(1:L/2); % from double to single sided FFT
+                SSB(2:end) = 2*SSB(2:end); % scaling step 1, multiply by 2 bins 2 to end (all except DC)
+                YFFT = abs(SSB/(L/2)); % scaling step 2, dividing by fft buffer size (L/2) (to be hoest i think this should be L)
+                fftPower = 2*(YFFT.^2); % this factor 2 is necessary to match power values from RCS
+                binStartBand1 = binStart(2*c-1);
+                binEndBand1 = binEnd(2*c-1);
+                binStartBand2 = binStart(2*c);
+                binEndBand2 = binEnd(2*c);
+                powerFromTimeDomain.(['PowerCh',num2str(2*c-1)])(stime+L-1) = sum(fftPower(binStartBand1:binEndBand1));
+                powerFromTimeDomain.(['PowerCh',num2str(2*c)])(stime+L-1) = sum(fftPower(binStartBand2:binEndBand2));
+            end
+            counter = counter + 1;
+            stime = stime + (L - ceil(L*overlap));
+        end   
+    end    
+    
+    powerOutput.Recum{inumRec} = inumRec;
+    powerOutput.Output{inumRec} = powerFromTimeDomain;
 end
 
-function [interval,binStart,binEnd,fftSize] = readFFTsettings(powerSettings,c)
+end
+
+
+function [interval,binStart,binEnd,fftSize] = readFFTsettings(powerSettings, inumRec)
     % at the moment assuming the only active Power Band is Band 0
-    interval = powerSettings.fftConfig.interval; % is given in ms
-    for iBand = 1:size(powerSettings.powerBands.indices_BandStart_BandStop,1)
-        binStart(iBand) = powerSettings.powerBands.indices_BandStart_BandStop(iBand,1);
-        binEnd(iBand) = powerSettings.powerBands.indices_BandStart_BandStop(iBand,2);
+    interval = powerSettings.fftConfig(inumRec).interval; % is given in ms
+    for iBand = 1:size(powerSettings.powerBands(inumRec).indices_BandStart_BandStop,1)
+        binStart(iBand) = powerSettings.powerBands(inumRec).indices_BandStart_BandStop(iBand,1);
+        binEnd(iBand) = powerSettings.powerBands(inumRec).indices_BandStart_BandStop(iBand,2);
     end
-    fftSize = powerSettings.fftConfig.size;
+    fftSize = powerSettings.fftConfig(inumRec).size;
 end
 
 % transform to rcs units (equation from manufacturer - hardware specific - same in all RC+S devices)
@@ -87,6 +111,4 @@ function td_rcs = transformTDtoRCS(keych,AmpGain)
     lfpGain_ch = 250*(config_trim_ch/255);  % actual gain amplifier ch
     lfp_rcs = lfp_mv * (lfpGain_ch*FP_READ_UNITS_VALUE) / (1000*1.2);
     td_rcs = lfp_rcs;
-end
-
 end
---
+++ code/hannWindow.m
@@ -0,0 +1,36 @@
+function hann_win = hannWindow(L,percentage)
+% calcualtes hann window given window size L and percentage (25%,50%,100%)
+% input
+% (1) L: window size (integer)
+% (2) Percentage: '25% Hann', '50% Hann' or '100% Hann' (string) (default, '100% Hann')
+
+if nargin ==0
+    error('you need to provide at least the number of window samples L')
+elseif nargin ==1
+    percentage = '100%';
+elseif nargin > 2
+    error('too many input arguments')
+end
+     
+switch percentage
+    case '100% Hann'
+        hann_win = 0.5*(1-cos(2*pi*(0:L-1)/(L-1))); % create hann taper function, equivalent to the Hann 100%         
+    case '50% Hann'
+        temp_win = 0.5*(1-cos(4*pi*(0:L-1)/(L-1))); % create hann taper function, equivalent to the Hann 100% 
+        hann_win = sethannwindow(temp_win);                
+    case '25% Hann'
+        temp_win = 0.5*(1-cos(8*pi*(0:L-1)/(L-1))); % create hann taper function, equivalent to the Hann 100% 
+        hann_win = sethannwindow(temp_win);  
+end
+
+end
+
+function hann_out = sethannwindow(temp_win)
+    % sets hann window flat top in case is not at 100% hann window 
+     [maxvals,indeces] = findpeaks(temp_win);
+     temp_win_middle = ones(1,length(indeces(1)-1:indeces(end)-1));
+     temp_win_left  = temp_win(1:indeces(1));
+     temp_win_right = temp_win(indeces(end):end);
+     hann_out = [temp_win_left temp_win_middle temp_win_right];        
+end
+        
