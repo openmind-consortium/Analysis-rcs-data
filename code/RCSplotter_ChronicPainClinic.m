d02a1897b446e61b739b57156721b624766df3c6
aschmitgen <59671209+aschmitgen@users.noreply.github.com>
2021-05-07 15:08:12 -0700

    Plot_LD

--- code/RCSplotter_ChronicPainClinic.m
+++ code/RCSplotter_ChronicPainClinic.m
@@ -5,10 +5,7 @@ rc = rcsPlotter();
 
 % RCS01
 
-%rc.addFolder('/Users/ashlynschmitgen/Documents/RCS01_Current/CPRCS01/Session1618120060929/DeviceNPC700435H/')
-
-%RCS02
-rc.addFolder('/Users/ashlynschmitgen/Documents/RCS02_Current/RCS02R/Session1619545769226/DeviceNPC700457H/')
+rc.addFolder('/Users/ashlynschmitgen/Documents/RCS01_Current/CL_050721/Session1620424608332/DeviceNPC700435H/')
 
 rc.loadData()
 
@@ -22,9 +19,17 @@ end
 rc.plotTdChannel(1,hsb(1,1)); title('TD Channel')
 rc.plotTdChannelSpectral(1,hsb(2,1)); title('Spectrogram')
 rc.plotActigraphyChannel('X',hsb(3,1)); title('Accelerometry')
-rc.plotAdaptiveLd(0, hsb(4,1)); title('LD0 Thresholds'); ylim([0 5000]);
+%rc.plotAdaptiveLd(0, hsb(4,1)); title('LD0 Thresholds'); ylim([0 50])
 rc.plotAdaptiveCurrent(0, hsb(5,1)); title('Current Delivered (mA)')
 rc.plotAdaptiveState(hsb(6,1)); title('Adaptive State');
+%%
+figure
+plot(AdaptiveData.Ld0_output); hold on;
+plot(mean(AdaptiveData.Ld0_output)); hold on;
+plot(AdaptiveData.Ld0_highThreshold, 'LineWidth', 2);
+plot(AdaptiveData.Ld0_lowThreshold, 'LineWidth', 2);
+title('LD0')
+ylim([0 50])
 
 % link axes since time domain and acc have differnt
 % sample rates: 
