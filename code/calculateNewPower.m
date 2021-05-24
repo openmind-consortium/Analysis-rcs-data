16e89d8a81de83f74d8cf993f2d3ad4e6697808c
Juan Anso <juan.anso@gmail.com>
2021-05-23 21:52:54 -0700

    Calculate power off-device

--- code/calculateNewPower.m
+++ code/calculateNewPower.m
@@ -1,5 +1,5 @@
-function [newPowerFromTimeDomain, newSettings] = calculateEquivalentDevicePower(combinedDataTable, fftSettings, powerSettings, metaData, channel, freqBand)
-% calculates equivalent device power as a function of chosen power bands
+function [newPower, newSettings] = calculateNewPower(combinedDataTable, fftSettings, powerSettings, metaData, channel, freqBand)
+% calculates a new fft band power series using the streamed time domain signal and based on new power and fft settings
 %
 % Assumption: only one set of fftSettings and powerSettings each time this function is invoked.
 % (1) Either the default (initial) settings of the recording session, or (see DEMO_CalculatePowerRCS.m, line~44)
@@ -7,7 +7,7 @@ function [newPowerFromTimeDomain, newSettings] = calculateEquivalentDevicePower(
 % 
 % Input = 
 % (1) combinedDataTable
-% (2) fftSettings: fft 
+% (2) fftSettings
 % (3) powerSettings 
 % (4) metaData (type = output from DEMO_Process)
 % (5) channel (type = integer (1..4), eg usage, channel = 1)
@@ -17,21 +17,23 @@ function [newPowerFromTimeDomain, newSettings] = calculateEquivalentDevicePower(
 % it, contact juan.ansoromeo@ucsf.edu or juan.anso@gmail.com
 
 % Parse input variables
-newSettings.fftSettings = fftSettings;
 newSettings.metaData = metaData;
 newSettings.tdChannel = channel;
 newSettings.bandLimits = freqBand;
 
 % initialize with the default sesttings to have access to default settings
-powerSettings(2:end,:) = [] ; % relying only on the first set of power settings (removing all other pwoer settings changes of the session)
+% relying only on first set of power and fft settings (removing all other settings changes of the session)
+powerSettings(2:end,:) = []; 
 newSettings.powerSettings = powerSettings;
+fftSettings(2:end,:) = [];
+newSettings.fftSettings = fftSettings;
 ampGains = newSettings.metaData.ampGains; % actual amplifier gains per channel
 
 % initialize output power table
-newPowerFromTimeDomain = table();
-newPowerFromTimeDomain.localTime= combinedDataTable.localTime;
-newPowerFromTimeDomain.DerivedTimes = combinedDataTable.DerivedTime;
-newPowerFromTimeDomain.calculatedPower = nan(1,size(combinedDataTable,1))';
+newPower = table();
+newPower.localTime= combinedDataTable.localTime;
+newPower.DerivedTimes = combinedDataTable.DerivedTime;
+newPower.calculatedPower = nan(1,size(combinedDataTable,1))';
 
 % initialize a newPowerSettings array
 newSettings.powerSettings.powerBands.indices_BandStart_BandStop(:,:) = []; 
@@ -58,7 +60,7 @@ disp('---')
 
 % add new bins information to new power band in power newSettings
 newSettings.powerSettings.powerBands.indices_BandStart_BandStop = [binIndecesInBand(1) binIndecesInBand(end)];
-newSettings.powerSettings.powerBands.powerBinsInHz = strcat(num2str(binsInBand(1)),'Hz-',num2str(binsInBand(end)),'Hz');
+newSettings.powerSettings.powerBands.powerBinsInHz = strcat(sprintf('%0.2f',binsInBand(1)),'Hz-',sprintf('%0.2f',binsInBand(end)),'Hz');
 
 % extract input parameters
 tch = combinedDataTable.DerivedTime;
@@ -72,7 +74,7 @@ switch fftSize % actual fft size of device for 64, 250, 1024 fftpoints
 end
 keych = combinedDataTable.(['TD_key',num2str(newSettings.tdChannel-1)]); % next channel
 td_rcs = transformTDtoRCS(keych,ampGains.(['Amp',num2str(newSettings.tdChannel)])); % transform TD signal to rcs internal values
-overlap = 1-(sr*interval/1e3/fftSizeActual); % time window parameters
+overlap = 1-((sr*interval/1e3)/fftSizeActual); % time window parameters
 L = fftSize; % timeWin is now named L, number of time window points
 hann_win = hannWindow(L,fftSettings.fftConfig.windowLoad);
 stime = 1; % sample 1 of data set where window starts
@@ -83,9 +85,9 @@ while counter <= totalTimeWindows % loop through time singal
         X = fft(td_rcs(stime:stime+L-1)'.*hann_win,fftSize); % fft of the next window
         SSB = X(1:L/2); % from double to single sided FFT
         SSB(2:end) = 2*SSB(2:end); % scaling step 1, multiply by 2 bins 2 to end (all except DC)
-        YFFT = abs(SSB/(L/2)); % scaling step 2, dividing by fft buffer size (L/2) (to be hoest i think this should be L)
+        YFFT = abs(SSB/(L/2)); % scaling step 2, dividing by fft buffer size (L/2)
         fftPower = 2*(YFFT.^2); % this factor 2 is necessary to match power values from RCS
-        newPowerFromTimeDomain.calculatedPower(stime+L-1) = sum(fftPower(binStart:binEnd));
+        newPower.calculatedPower(stime+L-1) = sum(fftPower(binStart:binEnd));
     end
     counter = counter + 1;
     stime = stime + (L - ceil(L*overlap));
